128e0b8ef10fa140dbf573dfc8b24cfd786a3917
Alfian Oktafireza <alfianokt104@gmail.com>
2021-01-17 09:54:35 +0700

    finish the game

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,6 +35,7 @@ const sections = {
   },
 };
 
+// show section by name or id
 const showPage = (name) => {
   Object.keys(sections).filter(el => el != '_el').forEach(el => {
     if (el == name) {
@@ -47,9 +48,15 @@ const showPage = (name) => {
   });
 };
 
-// show list flags
-const listFlags = document.querySelector('#list-flags');
-flags.emoji.forEach((el, index) => {
+/* start learn page */
+
+// show list flags on learn page
+const el_list_flags = document.querySelector('#list-flags');
+
+// count flags
+document.querySelector('#count-flags').innerText = flags.emoji.length;
+
+flags.list.forEach(el => {
   // what i want
   //
   // <li>
@@ -61,14 +68,19 @@ flags.emoji.forEach((el, index) => {
   const _h1 = document.createElement('h1');
   const _p = document.createElement('p');
 
-  _h1.innerText = el;
-  _p.innerText = flags.name.id[index];
+  _h1.innerText = el.emoji;
+  _p.innerText = el.name.id;
 
   _li.appendChild(_h1);
   _li.appendChild(_p);
 
-  listFlags.appendChild(_li);
+  el_list_flags.appendChild(_li);
 });
+
+/* end learn page */
+
+
+/* start router */
 
 router
   .on('/', () => {
@@ -96,3 +108,165 @@ Object.keys(routes).filter(el => el != '_el').forEach(el => {
 });
 
 router.listen();
+
+/* end router */
+
+
+/* start game page */
+
+// get random
+// Math.floor(Math.random()*flags.emoji.length);
+
+const btn_game_start = document.querySelector('#game-start');
+const el_game_time_left = document.querySelector('#game-time-left');
+const el_game_select = document.querySelector('#game-select');
+const el_game_title = document.querySelector('#game-title');
+const el_game_title_result = document.querySelector('#game-title-result');
+const el_game_title_played = document.querySelector('#game-title-played');
+const el_game_quest_title = document.querySelector('#game-quest-title');
+const el_game_total_answer = document.querySelector('#game-total-answer');
+const el_game_total_correct = document.querySelector('#game-total-correct');
+const el_game_to_start = document.querySelector('#game-to-start');
+
+let data_game = {
+  answered: {
+    // id: bool
+  },
+  now: {
+    id: null,
+    key: null,
+  },
+};
+
+
+// shuffle array
+Array.prototype.shuffle = function () {
+  return this.sort(() => Math.random() - 0.5);
+};
+// create question
+const createQuest = ()=>{
+  const flag_list = flags.list.filter(el => data_game.answered[el.id] == undefined || data_game.answered[el.id] == false);
+  const quest_length = 4;
+  const quest_list = flag_list.shuffle().slice(0, quest_length);
+  const key = Math.floor(Math.random() * (quest_length - 1));
+  console.log(key);
+
+  [data_game.now.id, data_game.now.key] = [quest_list[key].id, key];
+
+
+  // set quest title
+  el_game_select.innerText = quest_list[key].name.id;
+
+  // what i want
+  //
+  // <ul>
+  //  <li type="select" data-index="index">
+  //    <h1>flag</h1>
+  //  </li>
+  // </ul>
+  const _ul = document.createElement('ul');
+  quest_list.forEach((el, i) => {
+    const _li = document.createElement('li');
+    const _h1 = document.createElement('h1');
+
+    _h1.innerText = el.emoji;
+    _h1.dataset.index = i;
+
+    _li.dataset.index = i;
+    _li.type = 'select';
+    _li.append(_h1);
+    _ul.appendChild(_li);
+  });
+
+  el_game_select.append(_ul);
+
+  // create listener
+  const el_selector = document.querySelectorAll('#game-select > ul > li');
+  const eventHandler = ev => {
+    const user_feed = ev.target.dataset.index;
+    const user_feed_result = data_game.now.key == user_feed;
+
+    data_game.answered[data_game.now.id] = user_feed_result;
+
+    // if correct
+    if (user_feed_result) {
+      document.querySelector(`li[data-index="${user_feed}"]`).className = 's-1';
+    } else {
+      document.querySelector(`li[data-index="${user_feed}"]`).className = 's-0';
+      document.querySelector(`li[data-index="${data_game.now.key}"]`).className = 's-1';
+    }
+
+    // remove event
+    el_selector.forEach(el => {
+      el.removeEventListener('click', eventHandler);
+    });
+
+    setTimeout(() => {
+      // recursive
+      createQuest();
+    }, 500);
+  };
+
+  // add event listener
+  el_selector.forEach(el => {
+    el.addEventListener('click', eventHandler);
+  });
+}
+// game start
+btn_game_start.addEventListener('click', ()=>{
+  // hide game start title
+  el_game_title.classList.remove('db');
+  el_game_title.classList.add('dn');
+
+  // show game played title
+  el_game_title_played.classList.remove('dn');
+  el_game_title_played.classList.add('db');
+
+  let i = 60; // 60 second
+
+  // on game end
+  const gameEnd = ()=>{
+    const answered = data_game.answered;
+    el_game_total_answer.innerText = Object.keys(answered).length
+    el_game_total_correct.innerText = Object.keys(answered).filter(el => answered[el]).length;
+
+    // show el
+    el_game_title.classList.remove('db');
+    el_game_title.classList.add('dn');
+
+    el_game_title_played.classList.remove('db');
+    el_game_title_played.classList.add('dn');
+
+    el_game_title_result.classList.remove('dn');
+    el_game_title_result.classList.add('db');
+  };
+
+  // start game
+  const gameStart = ()=>{
+    el_game_time_left.innerText = i;
+    i--;
+
+    setTimeout(()=>{
+      // if < 0 end the game
+      // else recursive (call game start again)
+      i < 0 ? gameEnd() : gameStart();
+    }, 1000);
+  };
+
+  // call start game
+  createQuest();
+  gameStart();
+});
+
+el_game_to_start.addEventListener('click', ()=>{
+  el_game_title.classList.remove('dn');
+  el_game_title.classList.add('db');
+
+  el_game_title_played.classList.remove('db');
+  el_game_title_played.classList.add('dn');
+
+  el_game_title_result.classList.remove('db');
+  el_game_title_result.classList.add('dn');
+});
+
+/* end game page */
--- assets/js/flags.js
+++ assets/js/flags.js
@@ -3,4 +3,19 @@ const flags = {
   name: {
     id: "Pulau Ascension|Andorra|Uni Emirat Arab|Afganistan|Antigua & Barbuda|Anguilla|Albania|Armenia|Angola|Antartika|Argentina|Samoa Amerika|Austria|Australia|Aruba|Pulau Aland|Azerbaijan|Bosnia & Herzegovina|Barbados|Bangladesh|Belgia|Burkina Faso|Bulgaria|Bahrain|Burundi|Benin|St. Barthélemy|Bermuda|Brunei Darussalam|Bolivia|Karibia Belanda|Brazil|Bahama|Bhutan|Pulau Bouvet|Botswana|Belarusia|Belize|Kanada|Kepulauan Cocos (Keeling)|Kongo - Kinshasa|Republik Afrika Tengah|Kongo - Brazzaville|Swiss|Côte d'Ivoire|Kepulauan Cook|Chile|Kamerun|Cina|Kolumbia|Pulau Clipperton|Kosta Rika|Kuba|Tanjung Verde|Curacao|Pulau Natal|Siprus|Republik Ceko|Jerman|Diego Garcia|Djibouti|Denmark|Dominika|Republik Dominika|Aljazair|Ceuta & Melilla|Ekuador|Estonia|Mesir|Sahara Barat|Eritrea|Spanyol|Etiopia|Uni Eropa|Finlandia|Fiji|Kepulauan Falkland|Mikronesia|Kepulauan Faroe|Perancis|Gabon|Britania Raya|Grenada|Georgia|Guyana Perancis|Guernsey|Ghana|Gibraltar|Tanah penggembalaan|Gambia|Guinea|Guadeloupe|Guinea ekuator|Yunani|Georgia Selatan & Kepulauan Sandwich Selatan|Guatemala|Guam|Guinea-Bissau|Guyana|Hong Kong SAR Cina|Kepulauan Heard & McDonald|Honduras|Kroasia|Haiti|Hungaria|Pulau Canary|Indonesia|Irlandia|Israel|Pulau manusia|India|Wilayah Samudra Hindia Britania|Irak|Iran|Islandia|Italia|Jersey|Jamaika|Yordania|Jepang|Kenya|Kirgistan|Kamboja|Kiribati|Komoro|St. Kitts & Nevis|Korea Utara|Korea Selatan|Kuwait|Pulau cayman|Kazakhstan|Laos|Libanon|St. Lucia|Liechtenstein|Srilanka|Liberia|Lesotho|Lithuania|Luksemburg|Latvia|Libya|Maroko|Monaco|Moldova|Montenegro|St. Martin|Madagaskar|Pulau Marshall|Makedonia Utara|Mali|Myanmar (Burma)|Mongolia|Macao Sar Cina|Kepulauan Mariana Utara|Martinik|Mauritania|Montserrat|Malta|Mauritius|Maladewa|Malawi|Mexico|Malaysia|Mozambik|Namibia|Kaledonia Baru|Niger|Pulau Norfolk|Nigeria|Nikaragua|Belanda|Norway|Nepal|Nauru|Niue|Selandia Baru|Oman|Panama|Peru|Polinesia Perancis|Papua Nugini|Filipina|Pakistan|Polandia|St. Pierre & Miquelon|Kepulauan Pitcairn|Puerto Rico|Wilayah Palestina|Portugal|Palau|Paraguay|Qatar|Reuni|Rumania|Serbia|Rusia|Rwanda|Arab Saudi|Pulau Solomon|Seychelles|Sudan|Swedia|Singapura|St. Helena|Slovenia|Svalbard & Jan Mayen|Slowakia|Sierra Leone|San Marino|Senegal|Somalia|Suriname|Sudan Selatan|São Tomé & Príncipe|El Salvador|Sint Maarten|Suriah|Eswatini|Tristan Da Cunha|Kepulauan Turks & Caicos|Chad|Teritori Selatan Prancis|Untuk pergi|Thailand|Tajikistan|Tokelau|Timor-Leste|Turkmenistan|Tunisia|Tonga|Turki|Trinidad & Tobago|Tuvalu|Taiwan|Tanzania|Ukraina|Uganda|Kepulauan Terluar AS|Persatuan negara-negara|Amerika Serikat|Uruguay|Uzbekistan|Kota Vatikan|St. Vincent & Grenadines|Venezuela|Kepulauan Virgin Inggris|Kepulauan Virgin AS|Vietnam|Vanuatu|Wallis & Futuna|Samoa|Kosovo|Yaman|Mayotte|Afrika Selatan|Zambia|Zimbabwe|Inggris|Skotlandia|Wales|Texas (US-TX)".split('|'),
   },
+  get list() {
+    const data = [];
+
+    this.emoji.forEach((el, i) => {
+      data.push({
+        id: i+1,
+        emoji: el,
+        name: {
+          id: this.name.id[i],
+        },
+      })
+    });
+
+    return data;
+  },
 }
